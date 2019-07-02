33f742c943097f6c533671532236a1f170bb3c12
Subir Chandra Ghosh <subir0071@users.noreply.github.com>
2019-07-02 13:25:54 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,6 +4,12 @@
     these should be configurations common across 
     all apps governed by this config. 
 */
+
+application_environments {
+  dev {
+    long_name = "Development"
+  }
+}
 libraries{
   //merge = true 
   dotnet
